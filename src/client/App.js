2d222f5315c4266b04260c25e058f3adafbc22b6
Efim Pushkarev <pushkarevev@yandex.ru>
2020-08-05 20:44:02 +0300

    Добавил хэдер и поправил стили, чтоб он лучше вписывался к сайдменю и app-container

--- src/client/App.js
+++ src/client/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Table from './components/table/Table';
 import SideMenu from './components/sidemenu/SideMenu';
+import Header from './components/Header';
 
 const App = (props) => {
     return (
         <>
+            <Header />
             <SideMenu />
             <div className={'app-container'}>
                 <Table />
---
+++ src/client/components/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Header() {
+    return (<header><h1>Пушкарев Е.В. | Тестовое задание для Junior Frontend Developer</h1></header>)
+}

27ad8b23ecbb5320e5ab0d94a5b813b6247c7ef7
Efim Pushkarev <pushkarevev@yandex.ru>
2020-08-08 17:45:08 +0300

    Добавлен тест рендера компонента TableElement.js

---
+++ __tests__/components/table/TableElement.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow, render } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
+import TableElement from '../../../src/client/components/table/TableElement';
+import { initialState } from '../../../src/client/redux/initialState';
+
+const mockStore = configureMockStore();
+
+describe('<TableElement />', () => {
+    test('should render correctly without currentElement', () => {
+        const element = {
+            id: 1,
+            firstName: 'firstName',
+            lastName: 'lastName',
+            email: 'email',
+            phone: 'phone',
+        };
+        const elementIndex = 1;
+        const store = mockStore({ ...initialState });
+        const wrapper = shallow(
+            <TableElement store={store} element={element} elementIndex={elementIndex} />
+        );
+        const component = wrapper.dive().dive();
+        expect(component).toMatchSnapshot();
+    });
+    test('should render correctly with selected element', () => {
+        const element = {
+            id: 1,
+            firstName: 'firstName',
+            lastName: 'lastName',
+            email: 'email',
+            phone: 'phone',
+        };
+        const elementIndex = 0;
+        const store = mockStore({
+            ...initialState,
+            data: {
+                ...initialState.data,
+                currentElements: [{ id: 1 }],
+                currentElement: {
+                    elementIndex: 0,
+                    elementInfo: { id: 1 },
+                },
+            },
+        });
+        const wrapper = shallow(
+            <TableElement store={store} element={element} elementIndex={elementIndex} />
+        );
+        const component = wrapper.dive().dive();
+        expect(component).toMatchSnapshot();
+    });
+});

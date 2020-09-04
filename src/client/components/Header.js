b2fd6ad9f92c1e9c9247eca5dd1616b9e893b5db
Efim Pushkarev <pushkarevev@yandex.ru>
2020-09-04 19:55:57 +0300

    Поменял название проекта

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Header() {
-    return (<header><h1>Фильтрация и сортировка данных</h1></header>)
+    return (<header><h1>Контактный справочник</h1></header>)
 }
